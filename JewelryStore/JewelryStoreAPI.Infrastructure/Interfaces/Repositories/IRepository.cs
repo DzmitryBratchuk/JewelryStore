124f08279f9dcb948bd15a4648925c2ea9c39a9c
Dzmitry Bratchuk <Dzmitry.Bratchuk@itechart-group.com>
2020-01-24 16:59:10 +0300

    Added generic IRepository interface

---
+++ JewelryStore/JewelryStoreAPI.Infrastructure/Interfaces/Repositories/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JewelryStoreAPI.Infrastructure.Interfaces.Repositories
+{
+    public interface IRepository<TEntity> : IDisposable
+            where TEntity : class
+    {
+        Task<IQueryable<TEntity>> GetAll();
+        Task<TEntity> GetById(object id);
+        Task Create(TEntity entity, bool shouldSaveChanges = true);
+        Task Update(TEntity entity, bool shouldSaveChanges = true);
+        Task Delete(object id, bool shouldSaveChanges = true);
+        Task<int> SaveChanges();
+    }
+}
