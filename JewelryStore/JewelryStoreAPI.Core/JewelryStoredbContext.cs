6c6625bb7f13fb2975e063bce5a7d5826bfdb0af
Dzmitry Bratchuk <Dzmitry.Bratchuk@itechart-group.com>
2020-01-24 16:44:15 +0300

    Added JewelryStoredbContext

---
+++ JewelryStore/JewelryStoreAPI.Core/JewelryStoredbContext.cs
@@ -0,0 +1,25 @@
+﻿using JewelryStoreAPI.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JewelryStoreAPI.Core
+{
+    public class JewelryStoredbContext : DbContext
+    {
+        public JewelryStoredbContext(DbContextOptions<JewelryStoredbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<Bijouterie> Bijouteries { get; set; }
+        public virtual DbSet<PreciousMetalStuff> PreciousMetalStuffs { get; set; }
+        public virtual DbSet<Wristwatch> Wristwatchs { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<Basket> Baskets { get; set; }
+        public virtual DbSet<ProductBasket> ProductBaskets { get; set; }
+        public virtual DbSet<Order> Orders { get; set; }
+    }
+}
