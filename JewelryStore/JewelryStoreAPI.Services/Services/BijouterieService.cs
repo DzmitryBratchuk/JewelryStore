4d314ef08d0c6cea71f230b3d0f07349b94b6145
Dzmitry Bratchuk <Dzmitry.Bratchuk@itechart-group.com>
2020-01-24 17:56:16 +0300

    Added JewelryStoreAPI.Services project and BijouterieService

---
+++ JewelryStore/JewelryStoreAPI.Services/Services/BijouterieService.cs
@@ -0,0 +1,11 @@
+﻿using JewelryStoreAPI.Infrastructure.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JewelryStoreAPI.Services.Services
+{
+    class BijouterieService : IBijouterieService
+    {
+    }
+}
