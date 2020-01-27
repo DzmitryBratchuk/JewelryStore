35285d8e2ab06d9ef750dfea39f540715e78edd0
Dzmitry Bratchuk <Dzmitry.Bratchuk@itechart-group.com>
2020-01-27 13:51:12 +0300

    Added Configurations to Bijouterie and BijouterieType entities

---
+++ JewelryStore/JewelryStoreAPI.Core/Configurations/BijouterieConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using JewelryStoreAPI.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JewelryStoreAPI.Core.Configurations
+{
+    public class BijouterieConfiguration : IEntityTypeConfiguration<Bijouterie>
+    {
+        public void Configure(EntityTypeBuilder<Bijouterie> builder)
+        {
+            builder.Property(x => x.Name)
+                .IsRequired();
+
+            builder.HasOne(d => d.BijouterieType)
+                .WithMany(p => p.Bijouteries)
+                .HasForeignKey(d => d.BijouterieTypeId)
+                .HasConstraintName("FK_Bijouteries_To_BijouterieTypes")
+                .OnDelete(DeleteBehavior.Cascade)
+                .IsRequired();
+        }
+    }
+}
---
+++ JewelryStore/JewelryStoreAPI.Core/Configurations/BijouterieTypeConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using JewelryStoreAPI.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JewelryStoreAPI.Core.Configurations
+{
+    public class BijouterieTypeConfiguration : IEntityTypeConfiguration<BijouterieType>
+    {
+        public void Configure(EntityTypeBuilder<BijouterieType> builder)
+        {
+            builder.HasIndex(x => x.BijouterieTypeName)
+                .IsUnique();
+
+            builder.Property(x => x.BijouterieTypeName)
+                .IsRequired()
+                .HasMaxLength(20);
+        }
+    }
+}
